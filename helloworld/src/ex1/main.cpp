96e606d0e74885d6f74b1c107a26ef7a635688f3
Sergey Sh <bjolyk@gmail.com>
2019-10-28 04:23:16 +0300

    Initial.

---
+++ helloworld/src/ex1/main.cpp
@@ -0,0 +1,14 @@
+// include some definitions needed for woring with standard input/output (streams)
+#include <iostream>
+
+
+// the entrypoin of the application
+int main()
+{
+    // put a string literal "Hello world!" to the standard output stream in order 
+    // to print it at the console, which is normally associated with this stream
+    std::cout << "Hello world!";
+
+    // must return a "errorlevel code": 0 is for OK
+    return 0;
+}
